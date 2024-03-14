209ee098d1623745b1e911b1b0c26e8bffbf138b
Róbert Papp <robert.papp@magine.com>
2024-03-14 23:13:50 +0100

    Code cleanup

--- composeApp/src/commonMain/kotlin/presentation/screens/common/SuccessfulLoginScreen.kt
+++ composeApp/src/commonMain/kotlin/presentation/screens/common/SuccessfulLoginScreen.kt
@@ -1,18 +1,21 @@
 package presentation.screens.common
 
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.*
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
