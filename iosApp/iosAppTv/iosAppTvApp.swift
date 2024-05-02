023186a645a92b814c2749f980da1d08c4e2c36c
Róbert Papp <robert.papp@magine.com>
2024-05-02 12:43:16 +0200

    try to add tvOs to supported platforms

---
+++ iosApp/iosAppTv/iosAppTvApp.swift
@@ -0,0 +1,18 @@
+//
+//  iosAppTvApp.swift
+//  iosAppTv
+//
+//  Created by Robert Papp on 02/05/2024.
+//  Copyright © 2024 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct iosAppTvApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
